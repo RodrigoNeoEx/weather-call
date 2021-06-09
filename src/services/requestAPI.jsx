a4cd5da27b0b500839a2ee2c2acbed8325006973
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-09 10:53:55 -0300

    created request for city and state

--- src/services/requestAPI.jsx
+++ src/services/requestAPI.jsx
@@ -12,3 +12,19 @@ export async function requestCountry(input) {
   const country = await response.json();
   return console.log(country);
 }
+
+export async function requestStateByCountry(input) {
+  const response = await fetch(`https://api.countrystatecity.in/v1/countries/${input}/states`, requestOptions);
+  const state = await response.json();
+  return console.log(state);
+}
+
+export async function requestCity(input1, input2) {
+  const response = await fetch(
+    `https://api.countrystatecity.in/v1/countries/${input1}/states/${input2}/cities`,
+     requestOptions);
+  const city = await response.json();
+  return console.log(city);
+}
+
+
